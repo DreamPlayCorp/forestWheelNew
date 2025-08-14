49a48720d15e1c360e6d5306f290ab77093fdbcc
Volodymyr Voronkin <volodymyr.voronkin@Volodymyrs-MacBook-Pro.local>
2025-08-14 10:23:01 +0200

    fix

--- src/app/[locale]/(pages)/page.tsx
+++ src/app/[locale]/(pages)/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
                         <img src="/ezgif-2-572a4b516b%201.png" alt="" className="girl"/>
                     </div>
                     <p>
-                        500% Welcome Bonus up to 9 000 С$ and 777 Free Spins
+                        {t('prize')}
                     </p>
                 </div>
                 <a href={`https://dreamplay.bet/?registration=true&stag=${params.stag}&tracking_link=${params.tracking_link}`} className="button-claim">Claim</a>
